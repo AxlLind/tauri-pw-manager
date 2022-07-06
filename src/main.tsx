414e27a0d3199927c0c37b2675b36adbd1942def
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-07-06 22:09:01 +0200

    UI: Add basic main app structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { RecoilRoot, atom, useRecoilState } from 'recoil'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#649568',
+      main: '#149414',
+      dark: '#0e6b0e',
+    },
+  },
+});
+
+const pageState = atom({
+  key: 'page',
+  default: 'login',
+});
+
+function PageRouter() {
+  const [page, _] = useRecoilState(pageState);
+  switch (page) {
+  case 'login':
+    return <div>Click here to login</div>;
+  }
+  throw new Error(`Invalid page: ${page}`);
+}
+
+const App = () => (
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <RecoilRoot>
+        <PageRouter/>
+      </RecoilRoot>
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App/>);
