d039f04b76a7161453d0a04038e6d4d226fdcea8
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-07-14 21:30:34 +0200

    Backend: Impl error into userfacing error

--- src-tauri/src/error.rs
+++ src-tauri/src/error.rs
@@ -1,4 +1,4 @@
-use std::{fmt::Display, error::Error};
+use std::error::Error;
 use serde::{Serialize, ser::SerializeMap};
 
 #[derive(Debug, Clone)]
@@ -6,7 +6,7 @@ pub enum UserFacingError {
   InvalidCredentials,
   InvalidDatabase,
   UsernameTaken,
-  Unexpected(&'static str),
+  Unexpected,
 }
 
 impl UserFacingError {
@@ -15,7 +15,7 @@ impl UserFacingError {
       UserFacingError::InvalidCredentials => "invalid credentials",
       UserFacingError::InvalidDatabase => "corrupt key database",
       UserFacingError::UsernameTaken => "username already registered",
-      UserFacingError::Unexpected(reason) => reason,
+      UserFacingError::Unexpected => "unexpected error occurred"
     }
   }
 
@@ -24,23 +24,20 @@ impl UserFacingError {
       UserFacingError::InvalidCredentials => "invalid_credentials",
       UserFacingError::InvalidDatabase => "invalid_database",
       UserFacingError::UsernameTaken => "username_taken",
-      UserFacingError::Unexpected(_) => "unexpected",
+      UserFacingError::Unexpected => "unexpected",
     }
   }
 }
 
-impl Display for UserFacingError {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{}", self.message())
+impl<T: Error> From<T> for UserFacingError {
+  fn from(e: T) -> Self {
+    println!("Error: {}", e);
+    UserFacingError::Unexpected
   }
 }
 
-impl Error for UserFacingError {}
-
 impl Serialize for UserFacingError {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-      S: serde::Serializer {
+  fn serialize<S: serde::Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
     let mut json_err = serializer.serialize_map(Some(2))?;
     json_err.serialize_entry("key", self.key())?;
     json_err.serialize_entry("message", self.message())?;
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,26 +1,23 @@
-#![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
-)]
+#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
 mod database;
 mod cryptography;
 mod error;
+
 use std::fs;
 use std::path::{Path, PathBuf};
 use once_cell::sync::Lazy;
-use cryptography::EncryptedBlob;
-use database::CredentialsDatabase;
-
+use crate::cryptography::EncryptedBlob;
+use crate::database::CredentialsDatabase;
 use crate::error::UserFacingError;
 
 static APP_FOLDER: Lazy<PathBuf> = once_cell::sync::Lazy::new(|| {
   // TODO: Handle windows here
   let home_folder = std::env::var("HOME").expect("$HOME not set!");
-  let p = Path::new(&home_folder).join(".tauri-pw-manager");
-  if !p.exists() {
-    fs::create_dir(&p).expect("Could not create app folder");
+  let app_folder = Path::new(&home_folder).join(".tauri-pw-manager");
+  if !app_folder.exists() {
+    fs::create_dir(&app_folder).expect("Could not create app folder");
   }
-  p
+  app_folder
 });
 
 #[tauri::command]
@@ -30,11 +27,11 @@ fn login(username: String, password: String) -> Result<(), UserFacingError> {
   if !db_path.exists() {
     return Err(UserFacingError::InvalidCredentials);
   }
-  let bytes = fs::read(db_path).map_err(|_| UserFacingError::Unexpected("could not read the database file"))?;
+  let bytes = fs::read(db_path)?;
   let mut salt = [0; 12];
   salt.copy_from_slice(&bytes[0..12]);
-  let encrypted_blob = EncryptedBlob::from_bytes(&bytes[12..]).ok_or(UserFacingError::Unexpected("could not encrypt database file"))?;
-  let key = cryptography::pbkdf2_hmac(&password.as_bytes(), &salt);
+  let encrypted_blob = EncryptedBlob::from_bytes(&bytes[12..]).ok_or(UserFacingError::InvalidDatabase)?;
+  let key = cryptography::pbkdf2_hmac(password.as_bytes(), &salt);
   let decrypted_bytes = cryptography::decrypt(&key, &encrypted_blob).map_err(|_| UserFacingError::InvalidCredentials)?;
   let db = serde_json::from_slice::<CredentialsDatabase>(&decrypted_bytes).map_err(|_| UserFacingError::InvalidCredentials)?;
   println!("{:?}", db);
@@ -52,12 +49,12 @@ fn create_account(username: String, password: String) -> Result<(), UserFacingEr
     return Err(UserFacingError::UsernameTaken);
   }
   let salt = cryptography::random_bytes::<12>();
-  let key = cryptography::pbkdf2_hmac(&password.as_bytes(), &salt);
-  let db = database::CredentialsDatabase::new(username.clone());
-  let serialized_db = serde_json::to_string(&db).map_err(|_| UserFacingError::Unexpected("Could not serialize key database"))?;
-  let encrypted_blob = cryptography::encrypt(&key, serialized_db.as_bytes()).map_err(|_| UserFacingError::Unexpected("Failed to encrypt database"))?;
+  let key = cryptography::pbkdf2_hmac(password.as_bytes(), &salt);
+  let db = database::CredentialsDatabase::new(username);
+  let serialized_db = serde_json::to_vec(&db)?;
+  let encrypted_blob = cryptography::encrypt(&key, &serialized_db)?;
   let file_content = salt.into_iter().chain(encrypted_blob.iter()).collect::<Vec<_>>();
-  fs::write(db_path, &file_content).map_err(|_| UserFacingError::Unexpected("Could not write database to file"))?;
+  fs::write(db_path, &file_content)?;
   Ok(())
 }
 
