1b8ed527f074c282c44806e253b484ba11f18a68
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-07-27 13:26:02 +0200

    UI: Make user type password twice on account creation

--- src/SignUpPage.tsx
+++ src/SignUpPage.tsx
@@ -7,12 +7,15 @@ function SignUpPage({ goToPage }: PageProps) {
   const [error, setError] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [passwordCopy, setPasswordCopy] = useState('');
 
   const onClickLogin = async () => {
     if (username === '')
       return setError('Username missing.');
     if (password === '')
       return setError('Master password missing.');
+    if (password !== passwordCopy)
+      return setError('Passwords do not match');
     const err = await create_account(username, password);
     if (err)
       return setError(err.error);
@@ -25,6 +28,7 @@ function SignUpPage({ goToPage }: PageProps) {
       <Typography variant='h5'>Create an account</Typography>
       <TextField label='Username' value={username} onChange={e => setUsername(e.target.value)} />
       <TextField label='Master Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+      <TextField label='Retype Password' type='password' value={passwordCopy} onChange={e => setPasswordCopy(e.target.value)} />
       <Stack direction='row' spacing={1}>
         <Button variant='contained' onClick={onClickLogin}>Create</Button>
         <Button onClick={() => goToPage('login')}>Go Back</Button>
