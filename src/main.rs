214e4014e1ea3c3666ea0b54c51bcb2b3cfc7509
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-07-30 13:24:58 +0200

    Backend: Only add menu on macos

--- src/main.rs
+++ src/main.rs
@@ -206,7 +206,6 @@ fn main() {
 
   let context = tauri::generate_context!();
   tauri::Builder::default()
-    .menu(tauri::Menu::os_default(&context.package_info().name))
     .manage(Mutex::<Option<UserSession>>::default())
     .invoke_handler(tauri::generate_handler![
       create_account,
@@ -218,6 +217,11 @@ fn main() {
       generate_password,
       copy_to_clipboard,
     ])
+    .menu(if cfg!(target_os = "macos") {
+      tauri::Menu::os_default(&context.package_info().name)
+    } else {
+      tauri::Menu::default()
+    })
     .run(context)
     .expect("error while running tauri application");
 }
