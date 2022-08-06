19830d34b4136bb91e675b3c72b3ae358c08aab7
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-08-06 19:01:45 +0200

    UI: Refactor credentials overview page

--- ui/StartPage.tsx
+++ ui/StartPage.tsx
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
-import { Stack, Fab, Accordion, AccordionSummary, Typography, AccordionDetails, IconButton, Paper, Tooltip, Dialog, DialogContent, DialogActions, Button } from '@mui/material';
-import { Add, ExpandMore, Person, Key, Delete } from '@mui/icons-material';
+import { Stack, Fab, Typography, IconButton, Paper, Tooltip, Dialog, DialogContent, DialogActions, Button } from '@mui/material';
+import { Add, Person, Key, Delete } from '@mui/icons-material';
 import { useAsyncEffect, PageProps } from './utils';
 import { fetch_credentials, remove_credentials, copy_to_clipboard } from './backend';
 
 export function StartPage({ goToPage, showAlert }: PageProps) {
   const [credentials, setCredentials] = useState([] as string[]);
-  const [expanded, setExpanded] = useState('');
   const [credentialsToRemove, setCredentialsToRemove] = useState('');
 
   const populateCredentials = async () => {
@@ -37,28 +36,18 @@ export function StartPage({ goToPage, showAlert }: PageProps) {
       <Typography variant='h5' marginTop='2rem'>Credentials</Typography>
       {
         credentials.sort().map(name =>
-          <Accordion key={name} expanded={expanded === name} onChange={() => setExpanded(expanded === name ? '' : name)} disableGutters sx={{ width: '30rem' }} >
-            <AccordionSummary expandIcon={<ExpandMore/>}>
-              <Typography alignSelf='center' flexGrow={1}>{name}</Typography>
-              <Tooltip title='copy username'>
-                <IconButton children={<Person/>} onClick={e => copyValue(e, name, 'username')}/>
-              </Tooltip>
-              <Tooltip title='copy password'>
-                <IconButton children={<Key/>} onClick={e => copyValue(e, name, 'password')}/>
-              </Tooltip>
-              <Tooltip title='delete credentials'>
-                <IconButton children={<Delete/>} onClick={e => {e.stopPropagation(); setCredentialsToRemove(name);}}/>
-              </Tooltip>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Paper variant='outlined' sx={{ padding: '0.5rem' }}>
-                <Typography variant='overline'>Username</Typography>
-                <Typography noWrap>username</Typography>
-                <Typography variant='overline'>Password</Typography>
-                <Typography noWrap>password</Typography>
-              </Paper>
-            </AccordionDetails>
-          </Accordion>
+           <Paper sx={{ width: '30rem', display: 'flex', padding: '0.5rem 1rem' }} elevation={4}>
+            <Typography alignSelf='center' flexGrow={1}>{name}</Typography>
+            <Tooltip title='copy username'>
+              <IconButton children={<Person/>} onClick={e => copyValue(e, name, 'username')}/>
+            </Tooltip>
+            <Tooltip title='copy password'>
+              <IconButton children={<Key/>} onClick={e => copyValue(e, name, 'password')}/>
+            </Tooltip>
+            <Tooltip title='delete credentials'>
+              <IconButton children={<Delete/>} onClick={e => { e.stopPropagation(); setCredentialsToRemove(name); }}/>
+            </Tooltip>
+          </Paper>
         )
       }
     </Stack>
