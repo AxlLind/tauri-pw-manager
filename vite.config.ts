7f1a243d54a7f1d66b42ba16b2d49d73ee5a01bc
Axel Lindeberg <axel.ew.lindeberg@gmail.com>
2022-08-07 13:37:15 +0200

    Clean up vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,15 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   root: './ui',
   plugins: [ react() ],
+  server: {
+    host: '127.0.0.1',
+    port: 3000,
+    strictPort: true,
+  },
   build: {
     outDir: './build',
     emptyOutDir: true,
   },
   clearScreen: false,
+  logLevel: 'warn',
 });
